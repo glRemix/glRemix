0be7cdbb6752782f86bdef872084ee81bfab045c
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-12-06 15:24:56 -0500

    change index

--- glRemixRenderer/shaders/rt/raytracing.hlsl
+++ glRemixRenderer/shaders/rt/raytracing.hlsl
@@ -402,7 +402,7 @@ void ClosestHitMain(inout RayPayload payload, in TriAttributes attr)
     // Calculate which buffer and index within that buffer
     const uint instance_id = InstanceID();
 // TODO: Something here is wrong
-    const uint buffer_index = instance_id / MESHRECORDS_PER_BUFFER + g_raygen_cb.frame_index;
+    const uint buffer_index = instance_id / MESHRECORDS_PER_BUFFER * 2 + g_raygen_cb.frame_index;
     const uint local_index = instance_id % MESHRECORDS_PER_BUFFER;
     const GPUMeshRecord mesh = meshes[NonUniformResourceIndex(buffer_index)][local_index];
 
